faf977299c28a35ce00c70f7474782a236a351a1
Saad <sqfmason@yahoo.com>
2016-03-02 12:50:37 -0600

    added authentication in controller to check if current user is signed in

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -24,7 +24,7 @@ class PlacesController < ApplicationController
   # POST /places
   # POST /places.json
   def create
-    @place = Place.new(place_params)
+    @place = current_user.places.new(place_params)
 
     respond_to do |format|
       if @place.save
@@ -69,6 +69,6 @@ class PlacesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def place_params
-      params.require(:place).permit(:name, :address, :description, :phone, :website, :user_id)
+      params.require(:place).permit(:name, :address, :description, :phone, :website)
     end
 end
